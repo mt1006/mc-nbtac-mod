5036f8a38886919e87eeb91f963ca99855090c30
mt1006 <damianm916@gmail.com>
2023-08-02 23:12:42 +0200

    Version 1.1.3

--- src/main/java/com/mt1006/nbt_ac/autocomplete/loader/resourceloader/ResourceLoader.java
+++ src/main/java/com/mt1006/nbt_ac/autocomplete/loader/resourceloader/ResourceLoader.java
@@ -84,6 +84,7 @@ public class ResourceLoader extends SimplePreparableReloadListener<Map<ResourceL
 					else if (key.equals("operations")) { predictionPair.right = value.getAsJsonArray(); }
 					else if (key.startsWith("common/")) { common.add(new ImmutablePair<>(key, value.getAsJsonObject())); }
 					else if (key.startsWith("tag/")) { tags.add(new ImmutablePair<>(key, value.getAsJsonObject())); }
+					else if (key.startsWith("parent/")) { tags.add(new ImmutablePair<>(null, value.getAsJsonObject())); }
 				}
 
 				if (predictionPair.left != null && predictionPair.right != null) { predictions.add(predictionPair); }
