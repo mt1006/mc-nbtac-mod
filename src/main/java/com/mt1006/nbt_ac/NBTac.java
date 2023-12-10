268d2cec6e99c78ea2db9e819a8a7ab217d2dadb
mt1006 <damianm916@gmail.com>
2023-12-10 13:08:21 +0100

    Version for 1.20.4

--- src/main/java/com/mt1006/nbt_ac/NBTac.java
+++ src/main/java/com/mt1006/nbt_ac/NBTac.java
@@ -22,8 +22,8 @@ public class NBTac
 {
 	//TODO: add support for storage NBTs (1.2)
 	public static final String MOD_ID = "nbt_ac";
-	public static final String VERSION = "1.1.7";
-	public static final String FOR_VERSION = "1.20.2";
+	public static final String VERSION = "1.1.8";
+	public static final String FOR_VERSION = "1.20.4";
 	public static final String FOR_LOADER = "Forge";
 	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final boolean isDedicatedServer = FMLEnvironment.dist.isDedicatedServer();
