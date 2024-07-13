f00974f6e76da4ab2364db7674b187706591e780
mt1006 <damianm916@gmail.com>
2024-07-13 21:58:10 +0200

    Version 1.3.1

--- src/main/java/com/mt1006/nbt_ac/NBTac.java
+++ src/main/java/com/mt1006/nbt_ac/NBTac.java
@@ -24,7 +24,7 @@ import org.slf4j.Logger;
 public class NBTac
 {
 	public static final String MOD_ID = "nbt_ac";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.3.1";
 	public static final String FOR_VERSION = "1.20.1";
 	public static final String FOR_LOADER = "Forge";
 	public static final Logger LOGGER = LogUtils.getLogger();
