089c3e9d68626065f39b5e7a78776cc6346a1139
mt1006 <damianm916@gmail.com>
2024-07-22 15:52:04 +0200

    Fix compatibility issue with Lithium BlockEntity ticking cache (#28)

--- src/main/java/com/mt1006/nbt_ac/mixin/constructors/BlockEntityMixin.java
+++ src/main/java/com/mt1006/nbt_ac/mixin/constructors/BlockEntityMixin.java
@@ -13,7 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BlockEntity.class)
 public class BlockEntityMixin
 {
-	@Inject(method = "<init>", at = @At(value = "RETURN"))
+	// order = 500 is used, so it will be injected before Lithium's initSupportCache
+	// Lithium's mixin: https://github.com/CaffeineMC/lithium-fabric/blob/develop/src/main/java/me/jellysquid/mods/lithium/mixin/minimal_nonvanilla/world/block_entity_ticking/support_cache/BlockEntityMixin.java
+	// Issue: https://github.com/mt1006/mc-nbtac-mod/issues/28
+	@Inject(method = "<init>", at = @At(value = "RETURN"), order = 500)
 	private void atConstructor(BlockEntityType<?> blockEntityType, BlockPos blockPos,
 							   BlockState blockState, CallbackInfo ci) throws Exception
 	{
