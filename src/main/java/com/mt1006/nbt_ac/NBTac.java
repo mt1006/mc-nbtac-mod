86ccbcbcd9d750cc21b3914d1a649f742a5c1b10
mt1006 <damianm916@gmail.com>
2024-08-14 17:38:48 +0200

    Version 1.3.4

--- src/main/java/com/mt1006/nbt_ac/NBTac.java
+++ src/main/java/com/mt1006/nbt_ac/NBTac.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 public class NBTac implements ModInitializer
 {
 	public static final String MOD_ID = "nbt_ac";
-	public static final String VERSION = "1.3.3";
+	public static final String VERSION = "1.3.4";
 	public static final String FOR_VERSION = "1.19.2";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogUtils.getLogger();
