4fbcff7390ae0bdbfea1d6c54036dc43b711a5af
mt1006 <damianm916@gmail.com>
2024-08-14 17:32:16 +0200

    Fix ConfigScreenFactory

--- src/main/java/com/mt1006/nbt_ac/config/gui/ConfigScreenFactory.java
+++ src/main/java/com/mt1006/nbt_ac/config/gui/ConfigScreenFactory.java
@@ -1,13 +1,13 @@
 package com.mt1006.nbt_ac.config.gui;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class ConfigScreenFactory implements IConfigScreenFactory
 {
-	@Override public @NotNull Screen createScreen(@NotNull Minecraft minecraft, @NotNull Screen modListScreen)
+	@Override public @NotNull Screen createScreen(@NotNull ModContainer container, @NotNull Screen modListScreen)
 	{
 		// Needs to be separated from main class! In other case it crashed dedicated servers.
 		return new ConfigScreen(modListScreen);
