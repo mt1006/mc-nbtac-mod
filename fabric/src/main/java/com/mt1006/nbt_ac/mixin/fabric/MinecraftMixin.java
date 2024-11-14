1a87a223fd7d4b1b676756e91ca8f098c36e3d70
mt1006 <damianm916@gmail.com>
2024-11-15 00:28:07 +0100

    Version 1.3.8

---
+++ fabric/src/main/java/com/mt1006/nbt_ac/mixin/fabric/MinecraftMixin.java
@@ -0,0 +1,20 @@
+package com.mt1006.nbt_ac.mixin.fabric;
+
+import com.mt1006.nbt_ac.autocomplete.loader.Loader;
+import com.mt1006.nbt_ac.config.ModConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin
+{
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void atConstructor(GameConfig gameConfig, CallbackInfo ci)
+	{
+		if (ModConfig.useNewThread.val) { new Thread(Loader::load).start(); }
+	}
+}
