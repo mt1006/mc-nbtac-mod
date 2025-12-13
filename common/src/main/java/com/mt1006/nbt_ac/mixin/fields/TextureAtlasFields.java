4bad9589ff1db33f8233b4ebe179bcf1a5a0409a
mt1006 <mt1006@proton.me>
2025-12-13 20:50:19 +0100

    Add support for object text components (fix #63).

---
+++ common/src/main/java/com/mt1006/nbt_ac/mixin/fields/TextureAtlasFields.java
@@ -0,0 +1,15 @@
+package com.mt1006.nbt_ac.mixin.fields;
+
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(TextureAtlas.class)
+public interface TextureAtlasFields
+{
+	@Accessor Map<Identifier, TextureAtlasSprite> getTexturesByName();
+}
